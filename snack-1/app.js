214c6502eceb19f1097b85a1877471ee4f79a5ec
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-06-30 09:59:07 +0200

    feat: snack-1

--- snack-1/app.js
+++ snack-1/app.js
@@ -1,1 +1,13 @@
+const getPost = id => {
+    const promessa = new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(obj => resolve(obj.title))
+            .catch(reject)
+    });
+    return promessa
+};
 
+getPost(2)
+    .then(obj => console.log('titolo del libro : ', obj))
+    .catch(error => console.error(error))
